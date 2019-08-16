e77c9f1343ac8e4817005d414f5908ecc7408e5d
Andrey Lukashov <andrey.lukashov@gmail.com>
2019-08-16 04:03:46 -0700

    Reset the variable

--- instance/config.py
+++ instance/config.py
@@ -5,7 +5,7 @@ class Config(object):
     DEBUG = False
     CSRF_ENABLED = True
     SECRET = os.getenv('SECRET')
-    SQLALCHEMY_DATABASE_URI = 'postgres://taaiuzxnpynhvd:fca00cd20137a6cac1223b2fe199c24b87cbea54bb1bef07c93151f832f301fb@ec2-54-75-238-138.eu-west-1.compute.amazonaws.com:5432/ds7rvlbfqkfd2'
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
 
 
 class DevelopmentConfig(Config):
@@ -15,7 +15,7 @@ class DevelopmentConfig(Config):
 class TestingConfig(Config):
     DEBUG = True
     TESTING = True
-    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:123456@localhost/test_db'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://flaskapi:123456@localhost/test_db'
 
 
 class ProductionConfig(Config):
