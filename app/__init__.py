5938b84230f575f94a76e0de22da3c3f42edd77e
Andrey Lukashov <andrey.lukashov@gmail.com>
2019-08-16 03:40:43 -0700

    More tweaks for deployment

--- app/__init__.py
+++ app/__init__.py
@@ -11,7 +11,7 @@ def create_app(config_name):
     from app.models import Car, Branch, Driver
 
     app = FlaskAPI(__name__, instance_relative_config=True)
-    app.config.from_object('production')
+    app.config.from_object(app_config[config_name])
     app.config.from_pyfile('config.py')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.init_app(app)
--- run.py
+++ run.py
@@ -1,8 +1,7 @@
 import os
 from app import create_app
 
-config_name = os.getenv('APP_ENV')
-app = create_app(config_name)
+app = create_app('production')
 
 if __name__ == '__main__':
     app.run()
