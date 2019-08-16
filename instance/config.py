61450e6855fa3aec262d42fa7d6cbc835f7e30ea
Andrey Lukashov <andrey.lukashov@gmail.com>
2019-08-16 03:36:28 -0700

    database creds

--- instance/config.py
+++ instance/config.py
@@ -5,7 +5,7 @@ class Config(object):
     DEBUG = False
     CSRF_ENABLED = True
     SECRET = os.getenv('SECRET')
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
+    SQLALCHEMY_DATABASE_URI = 'postgres://taaiuzxnpynhvd:fca00cd20137a6cac1223b2fe199c24b87cbea54bb1bef07c93151f832f301fb@ec2-54-75-238-138.eu-west-1.compute.amazonaws.com:5432/ds7rvlbfqkfd2'
 
 
 class DevelopmentConfig(Config):
